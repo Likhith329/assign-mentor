8a0fce4c4aea159e7cf5e307c325cc73742b9229
Likhith kumar <likhithmattapalli@gmail.com>
2022-11-23 16:09:58 +0530

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const dotenv=require('dotenv')
 dotenv.config()
 
-const mongo=require('./connect')
-mongo.connect()
-
-const cors=require('cors')
-app.use(cors())
 const express=require('express')
 const app=express()
 app.use(express.json())
 
+const mongo=require('./connect')
+mongo.connect()
 
+
+const cors=require('cors')
+app.use(cors())
 
 const StudentRouter=require('./Router/StudentRouter')
 const MentorRouter=require('./Router/MentorRouter')
